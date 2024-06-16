f9eb9124141536221c0cf04e4de0bbda2ef369f0
Simeon Iksanov <Simeon.Iksanov@gmail.com>
2024-06-16 19:51:30 +0500

    modify

--- tests/Nutrifica.Application.UnitTests/Authentication/Refresh/RefreshTokensCommandHandlerTests.cs
+++ tests/Nutrifica.Application.UnitTests/Authentication/Refresh/RefreshTokensCommandHandlerTests.cs
@@ -29,6 +29,7 @@ public class RefreshTokensCommandHandlerTests
             "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiIyMzM4ZDNjMS05MWViLTQxYjQtODhmNi01ZGZiNTE3OTc0MzkiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6InVzZXJuYW1lIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZSI6ImZuIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvc3VybmFtZSI6Imxhc3RuYW1lIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiMSIsIm5iZiI6MTcxODQ2MDI1MiwiZXhwIjoxNzE4NDYwNTUyLCJpc3MiOiJJc3N1ZXIiLCJhdWQiOiJhdWRpZW5jZSJ9.iyrtP_5pFMT82mbeujcQjay8q4J_R5xpnkRh29C_OH4",
             "asdf",
             "1.1.1.1");
+
         _sut = new RefreshTokensCommandHandler(_jwtFactoryMock.Object, _userRepositoryMock.Object, _dateTimeProviderMock.Object, _unitOfWorkMock.Object);
     }
 
@@ -36,7 +37,9 @@ public class RefreshTokensCommandHandlerTests
     public async Task Handle_When_ProvidedJwtOfNotExistingUser_Should_ReturnNotFound()
     {
         // Arrange
-        _jwtFactoryMock.Setup(x => x.GetUserIdAsync(_command.Jwt)).ReturnsAsync(UserId.Empty);
+        _jwtFactoryMock
+            .Setup(x => x.GetUserIdAsync(_command.Jwt))
+            .ReturnsAsync(UserId.Empty);
 
         // Act
         var actual = await _sut.Handle(_command, default);
@@ -50,15 +53,9 @@ public class RefreshTokensCommandHandlerTests
     public async Task Handle_When_ProvidedRefreshTokenNotExists_Should_ReturnNotFound()
     {
         // Arrange
-        var user = User.Create("username", FirstName.Create("fn"), MiddleName.Create("mn"),
-            LastName.Create("lastname"), PhoneNumber.Create("5678"), Email.Create("email"), null);
-        user.Account.RefreshTokens = new List<RefreshToken> { new RefreshToken() {
-            Token = "aaaabbbbcccc"
-        } };
-        _jwtFactoryMock.Setup(x => x.GetUserIdAsync(_command.Jwt)).ReturnsAsync(user.Id);
-        _userRepositoryMock
-            .Setup(x => x.GetByIdWithRefreshTokensAsync(user.Id, default))
-            .ReturnsAsync(user);
+        var refreshTokens = new List<RefreshToken> { new RefreshToken() { Token = "aaaabbbbcccc" } };
+        var user = CreateUser(refreshTokens);
+        SetupMocks(user);
 
         // Act
         var actual = await _sut.Handle(_command, default);
@@ -72,16 +69,12 @@ public class RefreshTokensCommandHandlerTests
     public async Task Handle_When_ExpiredRefreshTokensProvided_Should_ReturnTokenExpired()
     {
         // Arrange
-        var user = User.Create("username", FirstName.Create("fn"), MiddleName.Create("mn"),
-            LastName.Create("lastname"), PhoneNumber.Create("5678"), Email.Create("email"), null);
-        user.Account.RefreshTokens = new List<RefreshToken> { new RefreshToken() {
+        var refreshTokens = new List<RefreshToken> { new RefreshToken() {
             Token = _command.RefreshToken,
             Expires = DateTime.UtcNow.AddMinutes(-20)
         } };
-        _jwtFactoryMock.Setup(x => x.GetUserIdAsync(_command.Jwt)).ReturnsAsync(user.Id);
-        _userRepositoryMock
-            .Setup(x => x.GetByIdWithRefreshTokensAsync(user.Id, default))
-            .ReturnsAsync(user);
+        var user = CreateUser(refreshTokens);
+        SetupMocks(user);
 
         // Act
         var actual = await _sut.Handle(_command, default);
@@ -95,19 +88,15 @@ public class RefreshTokensCommandHandlerTests
     public async Task Handle_When_RevokedRefreshTokensProvided_Should_ReturnTokenNotActive()
     {
         // Arrange
-        var user = User.Create("username", FirstName.Create("fn"), MiddleName.Create("mn"),
-            LastName.Create("lastname"), PhoneNumber.Create("5678"), Email.Create("email"), null);
-        user.Account.RefreshTokens = new List<RefreshToken> { new RefreshToken() {
+        var refreshTokens = new List<RefreshToken> { new RefreshToken() {
             Token = _command.RefreshToken,
             Expires = DateTime.UtcNow.AddMinutes(20),
             RevokedAt = DateTime.UtcNow.AddMinutes(-1),
             RevokedByIp = "2.2.2.2",
             ReasonRevoked = "test reasons",
         } };
-        _jwtFactoryMock.Setup(x => x.GetUserIdAsync(_command.Jwt)).ReturnsAsync(user.Id);
-        _userRepositoryMock
-            .Setup(x => x.GetByIdWithRefreshTokensAsync(user.Id, default))
-            .ReturnsAsync(user);
+        var user = CreateUser(refreshTokens);
+        SetupMocks(user);
 
         // Act
         var actual = await _sut.Handle(_command, default);
@@ -120,18 +109,13 @@ public class RefreshTokensCommandHandlerTests
     [Fact]
     public async Task Handle_When_RevokedRefreshTokensProvided_Should_RevokeAllChildTokens()
     {
-        // Arrange
-        var user = User.Create("username", FirstName.Create("fn"), MiddleName.Create("mn"),
-            LastName.Create("lastname"), PhoneNumber.Create("5678"), Email.Create("email"), null);
-        user.Account.RefreshTokens = new List<RefreshToken>{
+        var refreshTokens = new List<RefreshToken>{
             new RefreshToken() { Token = _command.RefreshToken, ReplacedByToken = "aaaaaa", RevokedAt= DateTime.UtcNow.AddMinutes(-1),RevokedByIp = "2.2.2.2", ReasonRevoked = "test reasons",},
             new RefreshToken() { Token = "aaaaaa", ReplacedByToken= "bbbbbb", RevokedAt= DateTime.UtcNow.AddMinutes(-1),RevokedByIp = "2.2.2.2", ReasonRevoked = "test reasons"},
             new RefreshToken() { Token = "bbbbbb"}
         };
-        _jwtFactoryMock.Setup(x => x.GetUserIdAsync(_command.Jwt)).ReturnsAsync(user.Id);
-        _userRepositoryMock
-            .Setup(x => x.GetByIdWithRefreshTokensAsync(user.Id, default))
-            .ReturnsAsync(user);
+        var user = CreateUser(refreshTokens);
+        SetupMocks(user);
         _dateTimeProviderMock.Setup(x => x.UtcNow).Returns(DateTime.UtcNow);
 
         // Act
@@ -145,9 +129,50 @@ public class RefreshTokensCommandHandlerTests
     [Fact]
     public async Task Handle_When_ValidRefreshTokensProvided_Should_ReturnRefreshedTokenPair()
     {
+        // Arrange
+        var refreshTokens = new List<RefreshToken> { new RefreshToken() {
+            Token = "asdf",
+            Expires = DateTime.UtcNow.AddMinutes(10),
+        } };
+        var user = CreateUser(refreshTokens);
+        SetupMocks(user);
+        _jwtFactoryMock
+            .Setup(x => x.GenerateAccessToken(user))
+            .Returns("jwt");
+        _jwtFactoryMock
+            .Setup(x => x.GenerateRefreshToken(_command.IpAddress))
+            .Returns(new RefreshToken { Token = "cccc" });
+
+        // Act
         var actual = await _sut.Handle(_command, default);
+
+        // Assert
         Assert.True(actual.IsSuccess);
         Assert.NotEmpty(actual.Value.Jwt);
         Assert.NotEmpty(actual.Value.RefreshToken);
+    }
+
+    private static User CreateUser(List<RefreshToken> refreshTokens)
+    {
+        var user = User.Create(
+            "username",
+            FirstName.Create("fn"),
+            MiddleName.Create("mn"),
+            LastName.Create("lastname"),
+            PhoneNumber.Create("5678"),
+            Email.Create("email"),
+            null);
+        user.Account.RefreshTokens = refreshTokens;
+        return user;
+    }
+
+    private void SetupMocks(User user)
+    {
+        _jwtFactoryMock
+            .Setup(x => x.GetUserIdAsync(_command.Jwt))
+            .ReturnsAsync(user.Id);
+        _userRepositoryMock
+            .Setup(x => x.GetByIdWithRefreshTokensAsync(user.Id, default))
+            .ReturnsAsync(user);
     }
 }
