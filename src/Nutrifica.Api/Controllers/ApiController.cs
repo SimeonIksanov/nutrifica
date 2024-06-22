8c13176a593d41f6916d8623b4dc01800b8a3048
Simeon Iksanov <Simeon.Iksanov@gmail.com>
2024-06-23 00:01:20 +0500

    Add Unauthorized error

--- src/Nutrifica.Api/Controllers/ApiController.cs
+++ src/Nutrifica.Api/Controllers/ApiController.cs
@@ -3,6 +3,7 @@ using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
+using Nutrifica.Domain.Aggregates.UserAggregate;
 using Nutrifica.Shared.Wrappers;
 
 namespace Nutrifica.Api.Controllers;
@@ -29,6 +30,12 @@ public class ApiController : ControllerBase
                         StatusCodes.Status400BadRequest,
                         result.Error,
                         validationResult.Errors)),
+            {Error.Code: "User.BadLoginOrPassword"} =>
+                Unauthorized(
+                    CreateProblemDetails(
+                        "Unauthorized",
+                        StatusCodes.Status401Unauthorized,
+                        result.Error)),
             _ => BadRequest(
                 CreateProblemDetails(
                     "Bad Request",
